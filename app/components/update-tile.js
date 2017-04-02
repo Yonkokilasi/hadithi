a9495ebe1461c2e49b4267b1ce39261db6c81848
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-02 19:39:22 +0300

    Update Question A queestion can now be updated when they click the update button . Functionality included

--- app/components/update-tile.js
+++ app/components/update-tile.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    updateQuestionForm: false,
+    actions: {
+        updateQuestion() {
+            this.set('updateQuestionForm',true);
+        },
+        update(question) {
+            var params = {
+                owner: this.get("owner"),
+                subject: this.get('subject'),
+                query: this.get('query'),
+        };
+        this.set('updateQuestionForm',false);
+        this.sendAction('update', question, params);
+    }
+},
 });
