e18f0e755a79d40c7650377c7affe8bbf0feb32a
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-04 10:52:22 +0300

    Fixed error that made the content not show on index Functinality of deleting does not work

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('question');
-  },
+    model() {
+      return Ember.RSVP.hash({
+        questions: this.store.findAll('question'),
+        answers: this.store.findAll('answer')
+      });
+    },
   actions: {
       update(question, params) {
         Object.keys(params).forEach(function(key) {
