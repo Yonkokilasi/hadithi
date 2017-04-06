a1b757c37ac4f91c77f095ee309592c227535fc3
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-06 23:18:10 +0300

    Added google fonts stylesheet Added add new question functionalty on index

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export function questionPopularity(params/*, hash*/) {
     var question = params[0];
-
+// Although at the moment the answers do not display , they are still counted . So for questions that have answers more than three, the below code will generate a glyphicon for it
     if(question.get('answers').get('length') >= 3) {
       return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
     }
+    // Although at the moment the answers do not display , they are still counted . So for questions that have answers less than three, the below code will generate a glyphicon for it
     if(question.get('answers').get('length') < 3) {
       return Ember.String.htmlSafe('<span class="glyphicon glyphicon-download"></span>');
   }
