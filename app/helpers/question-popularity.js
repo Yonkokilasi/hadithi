22738bd4627db8a6ef4f19def8970d6896e09fc1
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-06 22:55:36 +0300

    Added question popularity To help users know which questions have the most responses

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export function questionPopularity(params/*, hash*/) {
+    var question = params[0];
+
+    if(question.get('answers').get('length') >= 0) {
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    }
+  }
+
+
+export default Ember.Helper.helper(questionPopularity);
--- app/models/question.js
+++ app/models/question.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   owner: DS.attr(),
   subject: DS.attr(),
   query: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
