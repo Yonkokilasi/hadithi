11033fade487020f43ea52941feb892c1988ce29
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-06 23:01:12 +0300

    Added classifiation Questions that have less than 3 answers will display the download icon . The ones Above 3 will get a fire icon

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -3,9 +3,12 @@ import Ember from 'ember';
 export function questionPopularity(params/*, hash*/) {
     var question = params[0];
 
-    if(question.get('answers').get('length') >= 0) {
+    if(question.get('answers').get('length') >= 3) {
       return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
     }
+    if(question.get('answers').get('length') < 3) {
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-download"></span>');
+  }
   }
 
 
